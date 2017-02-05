c989b933e78eaf05b8b60cf72a578d3e41bea6e5
Kate Wannell <katefwannell7@gmail.com>
2017-02-04 23:05:53 -0800

    added functions for subtract,multiply and addition

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,33 @@ var add = function(number1,number2){
 	return number1 + number2;
 };
 
+var number1 = parseInt(prompt("What is number 1?"));
+var number2 = parseInt(prompt("What is number 2?"));
+alert ("Number 1 plus Number 2 is " + add(number1,number2));
+
+
+var subtract = function(number1,number2){
+	return number1 - number2;
+};
 
 var number1 = parseInt(prompt("What is number 1?"));
 var number2 = parseInt(prompt("What is number 2?"));
+alert ("Number 1 - Number 2 is " + subtract(number1,number2));
 
-alert (add(number1,number2));
+
+var multiply = function(number1,number2){
+	return number1 * number2;
+};
+
+var number1 = parseInt(prompt("What is number 1?"));
+var number2 = parseInt(prompt("What is number 2?"));
+alert ("Number 1 multiplied by Number 2 is " + multiply(number1,number2));
+
+
+var divide = function(number1,number2){
+	return number1 / number2;
+};
+
+var number1 = parseInt(prompt("What is number 1?"));
+var number2 = parseInt(prompt("What is number 2?"));
+alert ("Number 1 divided by Number 2 is " + divide(number1,number2));
