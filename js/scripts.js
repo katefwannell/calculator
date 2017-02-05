6141cc6050e36cb9da51e1190e24c3778ffe1af9
Kate Wannell <katefwannell7@gmail.com>
2017-02-04 22:49:25 -0800

    add new calculator project files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(number1,number2){
+	return number1 + number2;
+};
+
+
+var number1 = parseInt(prompt("What is number 1?"));
+var number2 = parseInt(prompt("What is number 2?"));
+
+alert (add(number1,number2));
